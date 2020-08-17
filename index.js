fe180db4d7354ddf9f372c5d0f22dd522486937c
Jan Andrle <andrle.jan@centrum.cz>
2020-08-17 13:47:33 +0200

    :boom: Relative paths support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const gulp_replace= require('gulp-replace');
 const fs= require('fs');
+const path = require('path');
 
 module.exports= function({ variable_eval= ()=> "", filesCleaner= content=> content, intendantion= "    " }= {}){
     let /* shared vars */
@@ -10,41 +11,54 @@ module.exports= function({ variable_eval= ()=> "", filesCleaner= content=> conte
         folder_glob_reg= /\*\*\/$/g,
         folder_deep_glob_reg= /\*\*\/\*\*\/$/g;
     const
-        processFiles= (is_once, folder, name, spaces)=> 
-            parseGlob(is_once, folder, ((name)=>[name, name.lastIndexOf("/")+1])(fileNameVarHandler(name)), spaces);
+        getFolderName= function(path){
+            const last_slash= path.lastIndexOf("/");
+            const folder_start= path.lastIndexOf("/", last_slash-1)+1;
+            return path.slice(folder_start, last_slash);
+        },
+        processFiles= (replaceHelper, is_once, folder, name, spaces)=> 
+            parseGlob(replaceHelper, is_once, folder, ((name)=>[name, name.lastIndexOf("/")+1])(nameVarHandler(name)), spaces);
     
     return function gulp_place({folder= "js/", string_wrapper= '"'}= {}){
-        const replaceHelper= fun=> function(...args){
+        const replaceHelper= (parent, fun)=> function(...args){
             const full_match= args.shift();
             const { spaces= "", name_1= false, name_2= false, type="file", semicol= "", jshint_global= "" }= args.pop();
-            return fun({ folder, string_wrapper, full_match, spaces, name: name_1||name_2, type, semicol, jshint_global, replaceHelper });
+            return fun({ parent, folder, string_wrapper, full_match, spaces, name: name_1||name_2, type, semicol, jshint_global, replaceHelper });
         };
         
-        return gulp_replace(gulp_place_regex, replaceHelper(parseFileHandler));
+        return gulp_replace(gulp_place_regex, replaceHelper("", parseFileHandler));
     };
-    function parseFileHandler({ folder, name, full_match, type, spaces, string_wrapper, semicol, jshint_global, replaceHelper }){
+    function parseFileHandler({ parent, folder, name, full_match, type, spaces, string_wrapper, semicol, jshint_global, replaceHelper }){
         if(!name) return full_match;
         name= name.replace(/&prime;/g, "'").replace(/&Prime;/g, "\"").replace(/`/g, "'");
         switch (type){
             case "clean":           return (files_added= new Set(), spaces+jshint_global);
             case "files":
-            case "glob":            return filesCleaner(parseFile(replaceHelper, processFiles(false, folder, name, spaces)));
+            case "glob":            return processFiles(replaceHelper, false, folder, fileNameVarHandler(name, parent), spaces);
             case "files_once":
-            case "glob_once":       return filesCleaner(parseFile(replaceHelper, processFiles(true, folder, name, spaces)));
-            case "file":            return fileHandler(replaceHelper, false, true, folder, fileNameVarHandler(name), spaces);
-            case "file_if_exists":  return fileHandler(replaceHelper, false, false, folder, fileNameVarHandler(name), spaces);
-            case "file_once":       return fileHandler(replaceHelper, true, true, folder, fileNameVarHandler(name), spaces);
-            case "js_bundle":       return parseJSBundle(replaceHelper, folder, fileNameVarHandler(name), spaces+intendantion);
+            case "glob_once":       return processFiles(replaceHelper, true, folder, fileNameVarHandler(name, parent), spaces);
+            case "file":            return fileHandler(replaceHelper, false, true, folder, fileNameVarHandler(name, parent), spaces);
+            case "file_if_exists":  return fileHandler(replaceHelper, false, false, folder, fileNameVarHandler(name, parent), spaces);
+            case "file_once":       return fileHandler(replaceHelper, true, true, folder, fileNameVarHandler(name, parent), spaces);
+            case "js_bundle":       return parseJSBundle(replaceHelper, parent, folder, nameVarHandler(name), spaces+intendantion);
             case "variable":        return spaces+string_wrapper+variable_eval(name)+string_wrapper+semicol+jshint_global;
             case "eval":            return (variable_eval(name), spaces+jshint_global);
             case "eval_out":        return spaces+variable_eval(name)+semicol+jshint_global;
         }
     }
-    function parseJSBundle(replaceHelper, folder, options, spaces){
-        const { src, name: name_candidate, type= "namespace", depends= [] }= JSON.parse(options) || {};
-        if(!src) return "";
-        const name= name_candidate || name_candidate.slice(name_candidate.lastIndexOf("/")+1, name_candidate.indexOf("."));
-        const content_candidate= fileHandler(replaceHelper, true, true, folder, src, spaces);
+    function parseJSBundle(replaceHelper, parent, folder, options, spaces){
+        const { src: src_candidate, src_file: src_file_candidate, name: name_candidate, type= "namespace", depends= [] }= JSON.parse(options) || {};
+        if(!src_candidate&&!src_file_candidate) return "";
+        let name, content_candidate;
+        if(src_file_candidate){
+            let src= fileNameVarHandler(src_file_candidate, parent);
+            name= name_candidate || src.slice(src.lastIndexOf("/")+1, src.indexOf("."));
+            content_candidate= fileHandler(replaceHelper, true, true, folder, src, spaces);
+        } else {
+            let src= fileNameVarHandler(src_candidate, parent);
+            name= name_candidate || getFolderName(src);
+            content_candidate= processFiles(replaceHelper, true, folder, src, spaces);
+        }
         if(!content_candidate) return "";
         const { content, exports }= parseModuleNamespaceExports(content_candidate);
         return require("./templates/"+type)(name, content, exports, depends);
@@ -59,18 +73,20 @@ module.exports= function({ variable_eval= ()=> "", filesCleaner= content=> conte
             .replace(/export (function|const|var|let|class) ([^ \=\-\+\(]+)/g, handleExportReplace);
         return { content, exports: Array.from(exports) };
     }
-    function parseFile(replaceHelper, file_data){
-        return file_data.replace(gulp_place_regex, replaceHelper(parseFileHandler));
+    function parseFile(replaceHelper, file_name, file_data){
+        const last_slash= file_name.lastIndexOf("/");
+        return file_data.replace(gulp_place_regex, replaceHelper(last_slash===-1 ? "" : file_name.slice(0, last_slash), parseFileHandler));
     }
     function fileHandler(replaceHelper, once, strict, folder, file_name, spaces){
-        if(once&&files_added.has(folder+file_name)) return "";
-        files_added.add(folder+file_name);
-        return parseFile(replaceHelper, spaces+filesCleaner(catFile(folder+file_name, strict)).replace(/\r?\n/gm, "\n"+spaces));
+        const path= folder+file_name;
+        if(once&&files_added.has(path)) return "";
+        files_added.add(path);
+        return parseFile(replaceHelper, file_name, spaces+filesCleaner(catFile(path, strict)).replace(/\r?\n/gm, "\n"+spaces));
     }
-    function parseGlob(once, main_folder, match, spaces){
+    function parseGlob(replaceHelper, once, main_folder, match, spaces){
         const [ name, last_slash ] = match;
         let [ sub_folder, files ] = [ name.substr(0, last_slash), name.substr(last_slash) ];
-        //if(!last_slash) return "";
+        if(!last_slash) return ""; //TODO: Why?
         files= new RegExp(files
             .replace(/[\.\(\)]/g, m=> "\\"+m)
             .replace(/\*/g, ".*")
@@ -81,10 +97,11 @@ module.exports= function({ variable_eval= ()=> "", filesCleaner= content=> conte
         return folders.reduce((acc, folder)=> acc+(acc?"\n":"")+parseFolder(folder), "");
 
         function parseFolder(folder){
-            return spaces+fs.readdirSync(folder)
-                    .filter(file_candidate=> files.test(file_candidate)&&(!once||!files_added.has(folder+file_candidate)))
-                    .map(file_name=> ( files_added.add(folder+file_name), catFile(folder+file_name).replace(/\r?\n/gm, "\n"+spaces) ))
-                    .join("\n");
+            return fs.readdirSync(folder)
+                .map(file_name=> folder.replace(main_folder, "")+file_name)
+                .filter(file_candidate=> files.test(file_candidate))
+                .map(file_name=> fileHandler(replaceHelper, once, false, main_folder, file_name, spaces))
+                .join("\n");
         }
         function getFolders(folders_pattern){
             const parent_folder= folders_pattern.replace(/\*\*\//g, "");
@@ -94,11 +111,14 @@ module.exports= function({ variable_eval= ()=> "", filesCleaner= content=> conte
             return first_deep.concat(first_deep.reduce((acc, folder)=> acc.concat(getFolders(subFoldersPattern(folder))), []));
         }
     }
-    function fileNameVarHandler(str){
+    function fileNameVarHandler(str, parent_folder){
+        const file_candidate= nameVarHandler(str);
+        const [ first_letter ]= file_candidate;
+        return first_letter==="." ? path.resolve(parent_folder, file_candidate).replace(path.resolve()+"\\", "").replace(/\\/g, "/") : file_candidate;
+    }
+    function nameVarHandler(str){
         if(typeof str !== "string") throw Error(`Type of '${str}' is not string!`);
-        const reg= /\$\{([\s]*[^;\s\{]+[\s]*)\}/g;
-        return str.replace(reg, replaceHandler);
-        function replaceHandler(_, match){return variable_eval(match);}
+        return str.replace(/\$\{([\s]*[^;\s\{]+[\s]*)\}/g, (_, match)=> variable_eval(match));
     }
     function catFile(file, strict){
         try{
